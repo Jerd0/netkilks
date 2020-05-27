3999908cf2cac21793be39baa1b1db0d28157065
jerd0 <Jerdo39@gmail.com>
2020-05-27 18:33:11 +0300

    update code, comment what don`t using

--- script/main.js
+++ script/main.js
@@ -1,14 +1,56 @@
-/*
-ДЗ №1 строка 32
-*/
+const IMG_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
+const API_KEY = "b7e11c9b9ca94287321e3d78cd528523";
 
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
+const tvShowsList = document.querySelector('.tv-shows__list')
+const modal = document.querySelector('.modal')
+
+const DBService = class {
+    getData = async url => {
+        const res = await fetch (url);
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error (`Не удалось получить данные по адресу ${url}`)
+        }
+    }
+    getTestData = () => {
+        return this.getData ('test.json')
+    }
+}
+const renderCard = response => {
+    tvShowsList.textContent='';
+    response.results.forEach(item => {
+        const {
+            backdrop_path: backdrop,
+            name: title,
+            poster_path: poster,
+            vote_average: vote
+        } = item;
+        const posterIMG = poster ? IMG_URL + poster : 'img/no-poster.jpg';
+        const backdropIMG = backdrop ? IMG_URL + backdrop : '';
+
+        const card = document.createElement ('li');
+        card.classList.add('tv-shows__item');
+        card.innerHTML = ` <a href="#" class="tv-card"><span class="tv-card__vote">${vote}</span>
+        <img class="tv-card__img" src="${posterIMG}"
+            data-backdrop="${backdropIMG}" alt="${title}">
+        <h4 class="tv-card__head">${title}</h4></a>`;
+    if (vote == '0') {
+      card.querySelector('.tv-card__vote').classList.add('hide')
+    };
+    tvShowsList.append(card);
+  });
+}
+new DBService().getTestData().then(renderCard);
+
+
+//Menu
 
 hamburger.addEventListener('click', () => {
     leftMenu.classList.toggle('openMenu');
     hamburger.classList.toggle('open');
-    
 });
 
 document.body.addEventListener('click', event => {
@@ -27,18 +69,33 @@ leftMenu.addEventListener('click', event => {
         hamburger.classList.add('open');
     }
 });
-
-function swapAttrs (elem) {
-    let tmp = elem.src
-    elem.setAttribute('src', elem.dataset.backdrop)
-    elem.setAttribute('data-backdrop', tmp)
+// swap picture
+const swapAttrs = elem => {
+    const card = event.target.closest('.tv-shows__item');
+    if (card) {
+        const img = card.querySelector('.tv-card__img');
+        if (img.dataset.backdrop) {
+            [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
+        }
+    }
 }
-
-Array.from(document.querySelectorAll ('.tv-card__img')).map( item => {
-    item.addEventListener('mouseover', event => {
-        swapAttrs(event.target)
-    })
-    item.addEventListener('mouseout', event => {
-        swapAttrs(event.target)
-    })
+tvShowsList.addEventListener('mouseover', swapAttrs);
+tvShowsList.addEventListener('mouseout', swapAttrs);
+// on modal window
+tvShowsList.addEventListener('click', event => {
+    event.preventDefault()
+    const target = event.target
+    const card = target.closest('.tv-card')
+    if (card) {
+        document.body.style.overfow='hidden'
+        modal.classList.remove('hide')
+    }
+})
+// on modal window
+modal.addEventListener('click', event => {
+    if (event.target.closest('.cross') ||
+        event.target.classList.contains('modal')) {
+        document.body.style.overflow=''
+        modal.classList.add('hide')
+    }
 })
